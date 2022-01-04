86e1b058fa0b5f1b8cb72d0a7ed4537a3d0f45b1
CSH <everybodypicks@outlookcom>
2022-01-04 22:26:21 +0800

    ss

---
+++ src/components/milesender.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="input">
+      <el-input id="url" v-model="url" placeholder="请输入地址" style="text-align:center;width: 200px"></el-input>
+      <el-input id="password" v-model="text" placeholder="请输入内容" style="text-align:center;width: 200px"></el-input>
+      <el-button @click="send">提交</el-button>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "milesender",
+  data(){
+    return{
+      url:'',
+      text:''
+    }
+  },
+  methods:{
+    send(){
+      axios({
+        method: 'post',
+        url: "http://127.0.0.1:8081/test/sendOne",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        data: {
+          url: this.url,
+          text: this.text
+        },
+      })
+      console.log(this.text)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'element-ui/lib/theme-chalk/index.css'
+import input from "element-ui/packages/input";
+import button from "element-ui/packages/button";
 Vue.config.productionTip = false
-
+Vue.use(input)
+Vue.use(button)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
